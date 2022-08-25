cca6d4862e9126653baf18671671a838654feac4
Aryan Kumar <kumar.aryan@gmail.com>
2022-08-24 20:02:53 -0700

    added links to all years of mvp voting to start_urls

--- bball_ref/spiders/bball.py
+++ bball_ref/spiders/bball.py
@@ -2,7 +2,17 @@ import scrapy
 
 class Bball(scrapy.Spider):
     name = 'bball'
-    start_urls = ['https://www.basketball-reference.com/players/']
+    start_urls = ['https://www.basketball-reference.com/awards/awards_2022.html#mvp',]
+
+    years = list(range(1991, 2021))
+    url_init = "https://www.basketball-reference.com/awards/awards_{}.html#mvp"
+
+    for year in years:
+        url = url_init.format(year)
+        start_urls.append(url)    
+    
+    print(start_urls)
+    print("")
 
     def parse(self, response):
         for link in response.css('li'):
